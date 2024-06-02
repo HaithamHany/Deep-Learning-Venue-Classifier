f1111e96873845df7a4cb93d5b99b3278bb98409
Haitham Abdel-Salam <haithamhany7@gmail.com>
2024-06-01 21:34:07 -0400

    Adding Config file

---
+++ Config.py
@@ -0,0 +1,10 @@
+# Configuration dictionary for hyperparameters
+config = {
+    "max_depth": [5, 10],
+    "min_samples_split": [2, 5],
+    "min_samples_leaf": [1, 2],
+    "random_state": 42,
+    "n_iter": 5,           # Reduced number of iterations
+    "cv": 3,               # Number of folds in cross-validation
+    "n_jobs": -1,          # Use all available CPU cores
+}
