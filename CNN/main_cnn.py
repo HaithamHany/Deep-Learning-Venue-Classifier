6694ab0c11e6a5b89b4ca38f04f6bafe169bfcfa
Haitham Abdel-Salam <haithamhany7@gmail.com>
2024-06-13 08:49:40 -0400

    Completing CNN

--- CNN/main_cnn.py
+++ CNN/main_cnn.py
@@ -1,9 +1,17 @@
+import os
+
+import torch
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader, random_split
+import torch.nn as nn
+from CNN import CNN
+num_epochs = 4
+num_classes = 10
+learning_rate = 0.001
 
-data_dir = '../dataset'
-def load_data(data_dir):
+def load_data():
+    data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'dataset'))
     # preprocessing
     transform = transforms.Compose([
         transforms.Resize((32, 32)),  # Resize images to 32x32 if they are not already
@@ -27,3 +35,57 @@ def load_data(data_dir):
     classes = sorted([d.name for d in os.scandir(data_dir) if d.is_dir()])
 
     return train_loader, test_loader, classes
+
+def cnn():
+    model = CNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    train_loader, test_loader, classes = load_data()
+
+    # Total steps per epoch
+    total_step = len(train_loader)
+    loss_list = []
+    acc_list = []
+
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):  # Iterate batches
+            # Forward pass
+            outputs = model(images)  # Compute predictions
+            loss = criterion(outputs, labels)  # Calculate loss
+            loss_list.append(loss.item())  # Record loss
+
+            # Backprop and optimisation
+            optimizer.zero_grad()  # Clear gradients
+            loss.backward()  # Compute gradients
+            optimizer.step()  # Update weights
+
+            # Train accuracy
+            total = labels.size(0)  # Total labels
+            _, predicted = torch.max(outputs.data, 1)  # Predicted labels
+            correct = (predicted == labels).sum().item()  # Correct predictions
+            acc_list.append(correct / total)  # Record accuracy
+
+            if (i + 1) % 100 == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
+                      .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(),
+                              (correct / total) * 100))
+
+    # Set model to evaluation mode
+    model.eval()
+    # Disable gradient computation
+    with torch.no_grad():
+        correct = 0
+    total = 0
+    # Iterate test batches
+    for images, labels in test_loader:
+        # Compute predictions
+        outputs = model(images)
+        # Get predicted labels
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum().item()
+    print('Test Accuracy of the model on the 10000 test images: {} %'
+          .format((correct / total) * 100))
+
+if __name__ == "__main__":
+    cnn()
