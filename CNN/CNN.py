437818e1bc1e9cc381d48293de281bfcc6054ee9
Haitham Abdel-Salam <haithamhany7@gmail.com>
2024-06-22 16:18:36 -0400

    Dynamically calculate the input size for the fully connected layer in CNN

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from Config import config_cnn_architecture
 
 class CNN(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size=128):
         super(CNN, self).__init__()
         layers = []
 
@@ -25,10 +25,13 @@ class CNN(nn.Module):
             if (i + 1) % 2 == 0:
                 layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
 
+        conv_output_size = calculate_conv_output_size(input_size, config_cnn_architecture)
+        fc_input_size = conv_output_size * conv_output_size * config_cnn_architecture["num_filters"][-1]
+
         self.conv_layer = nn.Sequential(*layers)
         self.fc_layer = nn.Sequential(
             nn.Dropout(p=0.1),
-            nn.Linear(3 * 3 * config_cnn_architecture["num_filters"][-1], 1000),
+            nn.Linear(fc_input_size, 1000),
             nn.ReLU(inplace=True),
             nn.Linear(1000, 512),
             nn.ReLU(inplace=True),
@@ -44,3 +47,14 @@ class CNN(nn.Module):
         # fc layer. Passes the flattened data through the fully connected layers.
         x = self.fc_layer(x)
         return x
+
+
+
+
+def calculate_conv_output_size(input_size, config):
+    size = input_size
+    for i in range(config["num_layers"]):
+        size = (size - config["filter_sizes"][i] + 2 * config["paddings"][i]) // config["strides"][i] + 1
+        if (i + 1) % 2 == 0:  # Apply max pooling after every two layers
+            size = size // 2
+    return size
